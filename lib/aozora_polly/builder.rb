ff23d3ff5ef25cd69cf14ac0cadb34e9a91fe279
hogelog <konbu.komuro@gmail.com>
2017-01-18 04:06:15 +0900

    Add bibliography

--- lib/aozora_polly/builder.rb
+++ lib/aozora_polly/builder.rb
@@ -11,13 +11,15 @@ module AozoraPolly
 
     def html2ssml(html)
       doc = ::AozoraPolly::Document.parse(html.encode("utf-8"))
-      ssml_body = create_body_ssml(doc)
-      create_ssml(doc, ssml_body)
+
+      main_ssml = html2ssml_fragment(doc.main_text)
+      bib_ssml = html2ssml_fragment(doc.bibliography)
+      create_ssml(doc, main_ssml + bib_ssml)
     end
 
-    def create_body_ssml(doc)
+    def html2ssml_fragment(fragment)
       Sanitize.
-        fragment(doc.main_text, elements: %w(br ruby rb rp rt div h1 h2 h3 h4 h5)).
+        fragment(fragment, elements: %w(br ruby rb rp rt div h1 h2 h3 h4 h5)).
         gsub(%r(<br\s*/?>), '<break />').
         gsub(%r(<(?:h\d|div)[^>]*>), '<p>').
         gsub(%r(</(?:h\d|div)>), '</p>').
--- lib/aozora_polly/document.rb
+++ lib/aozora_polly/document.rb
@@ -2,20 +2,22 @@ require "nokogiri"
 
 module AozoraPolly
   class Document
-    attr_reader :title, :author, :main_text
+    attr_reader :title, :author, :main_text, :bibliography
 
     def self.parse(html)
       doc = Nokogiri::HTML.parse(html)
       title = doc.xpath('//h1').text
       author = doc.xpath('//h2').text
       main_text = doc.xpath('//div[@class="main_text"]').to_html
-      new(title, author, main_text)
+      bibliography = doc.xpath('//div[@class="bibliographical_information"]').to_html
+      new(title, author, main_text, bibliography)
     end
 
-    def initialize(title, author, main_text)
+    def initialize(title, author, main_text, bibliography)
       @title = title
       @author = author
       @main_text = main_text
+      @bibliography = bibliography
     end
   end
 end
